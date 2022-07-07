36a8c40412aee0072d1ebe01c315baec23726565
Monerovirus <monerovirus@gmx.com>
2022-07-07 16:54:27 +0100

    improved tryParseResponse for monero rpc

--- monero_rpc_tasks.py
+++ monero_rpc_tasks.py
@@ -6,10 +6,13 @@ API_URL = "http://localhost:28088/json_rpc"
 
 def tryParseResponse(resp):
     try:
-        return resp.json()
+        result = resp.json()
+        if 'error' in result:
+            return {"Error": result['error']}
+        return result
     except JSONDecodeError as e:
         logging.error(f"Failed to parse monero-rpc response: {resp}")
-        return False
+        return {"Error": "Failed to parse monero-rpc response."}
 
 def mPostRequest(data):
     data['jsonrpc'] = "2.0"
@@ -21,7 +24,7 @@ def mPostRequest(data):
 def mGetBalances(asset):
     data = {'method': "get_accounts"}
     resp = tryParseResponse(mPostRequest(data))
-    if resp == False:
-        return {"Error": "Failed to parse monero-rpc response."}
+    if 'Error' in result:
+        return result
 
     return {"balance": resp['result']['total_balance'], "unlocked_balance": resp['result']['total_unlocked_balance']}

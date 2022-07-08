3102bef09d7dad6879e2d101ca425d566cf5aa5a
Monerovirus <monerovirus@gmx.com>
2022-07-08 17:28:26 +0100

    added mVerifyBalance

--- monero_rpc_tasks.py
+++ monero_rpc_tasks.py
@@ -14,22 +14,45 @@ def tryParseResponse(resp):
         logging.error(f"Failed to parse monero-rpc response: {resp}")
         return {"Error": "Failed to parse monero-rpc response."}
 
-def mPostRequest(data):
+def postRequest(data):
     data['jsonrpc'] = "2.0"
     data['id'] = "0"
     return requests.post(API_URL, data=json.dumps(data))
 
+def moneroToAtomic(moneroAmount):
+    return int(moneroAmount * 1_000_000_000_000)
+
+def atomicToMonero(atomicAmount):
+    return atomicAmount / 1_000_000_000_000
+
 def mGetBalances():
     data = {'method': "get_accounts"}
-    resp = tryParseResponse(mPostRequest(data))
+    resp = tryParseResponse(postRequest(data))
     if 'Error' in resp:
         return resp
 
     return {"balance": resp['result']['total_balance'], "unlocked_balance": resp['result']['total_unlocked_balance']}
 
-def mTransfer(address, atomicAmount):
+def mVerifyBalance(requiredBalance, retryCount, waitSeconds):
+    atomicRequiredBalance = moneroToAtomic(requiredBalance)
+    tryCount = 0
+    result = None
+    while tryCount < retryCount:
+        logging.debug(f"Attempting verify unlocked balance is {requiredBalance} or higher.")
+        result = mGetBalances()
+        if 'Error' in result:
+            return result
+        balance = result['unlocked_balance']
+        logging.debug(f"Unlocked balance was {atomicToMonero(balance)}.")
+        if balance >= atomicRequiredBalance:
+            return { "Balance" : balance }
+        tryCount += 1
+        time.sleep(waitSeconds)
+    return {"Error": f"Could not verify unlocked balance was {requiredBalance} after {retryCount} tries.\n{result}"}
+
+def mTransfer(address, amount):
     params = {
-        'destinations': [{'amount': atomicAmount, 'address': address}],
+        'destinations': [{'amount': moneroToAtomic(amount), 'address': address}],
         'priority': 1,
         'unlock_time': 10
         }
@@ -37,7 +60,7 @@ def mTransfer(address, atomicAmount):
         'method': "transfer",
         'params': params
         }
-    resp = tryParseResponse(mPostRequest(data))
+    resp = tryParseResponse(postRequest(data))
     if 'Error' in resp:
         return resp
 

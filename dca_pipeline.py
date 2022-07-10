9e8703f74c3a030d20ae7e7220167f828ca24195
Monerovirus <monerovirus@gmx.com>
2022-07-10 22:28:57 +0100

    typo

--- dca_pipeline.py
+++ dca_pipeline.py
@@ -25,7 +25,7 @@ def runTasks(amount):
     krakenCrypto = settings['kraken_crypto']
     fixedfloatFromCrypto = settings['fixedfloat_from_crypto']
     fixedfloatToCrypto = settings['fixedfloat_to_crypto']
-    finalCryptoAddress = setting['final_crypto_address_key']
+    finalCryptoAddress = settings['final_crypto_address_key']
     fiat = settings['kraken_fiat']
 
     pair = krakenCrypto + fiat
